1ea24dfff40a0534bcba72c5954a979d37525bfa
mnbvcxz567 <22011112@c.anabuki-college.net>
2023-07-27 19:25:51 +0900

    適切なコミットコメント

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest; 
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view("posts/index")->with(["posts" => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
     public function show(Post $post)
-{
-    return view('posts.show')->with(['post' => $post]);
- //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
-}
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+
     public function create()
-{
-    return view('posts/create');
+    {
+        return view('posts.create');
+    }
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
-}
+
