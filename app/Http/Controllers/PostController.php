f97cf24986d72d85b36dc66c0b09505a8ee0ba19
mnbvcxz567 <22011112@c.anabuki-college.net>
2023-08-10 23:49:18 +0900

    編集内容の登録

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,31 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
 use App\Http\Requests\PostRequest; 
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
 
     public function show(Post $post)
     {
-        return view('posts.show')->with(['post' => $post]);
+        return view('posts/show')->with(['post' => $post]);
     }
 
     public function create()
     {
-        return view('posts.create');
+        return view('posts/create');
     }
 
-    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    public function store(PostRequest $request,Post $post) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function edit(Post $post)
+{
+    return view('posts.edit')->with(['post' => $post]);
+}
+
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+}
 }
 
