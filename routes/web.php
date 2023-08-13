6ec4ffe260a039b4ebad5957a1e4df4a97bd616a
mnbvcxz567 <22011112@c.anabuki-college.net>
2023-08-14 04:25:55 +0900

    リレーション

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,4 @@ Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
 Route::delete('/posts/{post}', [PostController::class,'delete']);
 
+Route::get('/categories/{category}', [CategoryController::class,'index']);
